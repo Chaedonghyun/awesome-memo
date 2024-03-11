56c71508e1f4f7642d11be52cb35edeac24c7dbf
Chaedonghyun <102035495+Chaedonghyun@users.noreply.github.com>
2024-03-11 20:33:03 +0900

    PUT, DELETE 기능 추가 및

--- main.py
+++ main.py
@@ -18,5 +18,21 @@ def create_memo(memo:Memo):
 def  read_memo():
     return memos
 
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id==req_memo.id:
+            memo.content=req_memo.content
+            return "succes"
+    return "fail"
+            
+@app.delete("/memos/{memo_id}")
+def  delete_memo(memo_id):
+       for index,memo in enumerate(memos):
+          if memo.id==memo_id:
+              memos.pop(index)
+              return "succes"
+          return"fail"  
+
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
